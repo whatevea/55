c74017557f3119846d61309c88738b67ad3991f7
bibash thapa <bibash.thapa7@gmail.com>
2024-02-28 12:38:38 +0545

    Footer

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Navbar } from './Navbar'
+import Footer from './Footer'
 
 const Homepage = () => {
     return (
         <div>
             <Navbar />
             Homepage
+            <Footer/>
         </div>
     )
 }
