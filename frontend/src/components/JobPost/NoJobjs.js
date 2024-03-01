32a1640440d8dbd09dd727ac45a9d12fcd56f75a
bibash thapa <bibash.thapa7@gmail.com>
2024-03-01 16:45:15 +0545

    working on job search component

--- frontend/src/components/JobPost/NoJobjs.js
+++ frontend/src/components/JobPost/NoJobjs.js
@@ -2,16 +2,19 @@ import React from "react";
 
 export default function NoJobjs() {
   return (
-    <div className="m-8 py-3">
-      <div className="p-10 m-5 bg-gray-100 rounded-2xl">
+    <div className="md:m-8 md:py-3">
+      <div className="md:p-10 md:m-5 p-6 m-3 bg-gray-100 rounded-2xl">
         <div className="grid place-content-center gap-8">
-          <p className="flex justify-center text-2xl font-semibold">
+          <p className="flex justify-center text-sm sm:text-base md:text-2xl font-semibold">
             There are no active contracts.
           </p>
-          <p className="">
+          <p className="text-xs md:text-base">
             Contracts you're actively working on will appear here.
           </p>
-          <button className="bg-green-600 hover:bg-green-500 text-white p-2 px-5 rounded-3xl">
+        </div>
+
+        <div className="grid place-content-center md:p-4 p-3">
+          <button className="bg-green-600  hover:bg-green-500 text-white md:p-3 p-1 text-xs md:text-base rounded-3xl">
             Search for new projects
           </button>
         </div>
