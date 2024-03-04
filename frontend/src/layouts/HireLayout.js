83d1479583fd83748c20d55359603543ff581c6d
suraj-thadarai <42627217+suraj-thadarai@users.noreply.github.com>
2024-03-04 18:58:44 +0545

    joblist made responsive

--- frontend/src/layouts/HireLayout.js
+++ frontend/src/layouts/HireLayout.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function HireLayout() {
     return (
         <div>
-            HireLayout is here
+            
             <Outlet />
 
         </div>
