d0afe3c15c0b13cd1f4bb4d7eda0a838c5e0cb4a
bibash thapa <bibash.thapa7@gmail.com>
2024-03-04 18:02:29 +0545

    improved UI

--- frontend/src/components/JobPost/NoJobjs.js
+++ frontend/src/components/JobPost/NoJobjs.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NoJobjs() {
   return (
-    <div className="md:m-8 m-3 py-0 md:py-3">
+    <div className="m-3 py-0 md:py-3">
       <div className="md:p-10 md:m-5 p-6 m-3 grid md:gap-8 bg-gray-100 rounded-2xl">
         <div className="grid place-content-center">
           <p className="flex justify-center text-sm sm:text-base md:text-2xl font-semibold">
@@ -11,8 +12,8 @@ export default function NoJobjs() {
         </div>
 
         <div className="grid place-content-center md:p-4 p-2">
-          <button className="bg-green-600  hover:bg-green-500 text-white md:p-3 p-1 w-full text-xs md:text-base rounded-3xl">
-            Search for new projects
+          <button className="bg-green-600  hover:bg-green-500 text-white md:p-3 p-2 w-full text-xs md:text-base rounded-md">
+            <Link to="/searchjobs">Search for new projects</Link>
           </button>
         </div>
       </div>
